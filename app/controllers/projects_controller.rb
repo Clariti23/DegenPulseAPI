05acc30b041b341054679f86ce4d5486c83af0e2
clariti23 <g.ferowich@gmail.com>
2020-10-16 15:12:28 -0500

    introduce routes and CRUD controller actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,38 @@
 class ProjectsController < ApplicationController
+    
+    # Index of all defi and degen projects
+    def index 
+        projects = Project.all 
+        render json: projects
+    end
+
+    def show
+        project = Project.find(params[:id])
+        render json: project
+    end
+    
+    def create
+        project = Project.create(project_params)
+        puts project
+    end
+
+    def update
+        project = Project.find(params[:id])
+        project.update(project_params)
+        render json: project
+    end    
+
+    def destroy
+        project = Project.find(params[:id])
+        project.destroy
+        render json: projects
+    end
+    
+    private 
+    def project_params
+        params.require(:project).permit(:name, :chain, :category, :held)
+    end
+end
+
+
 end
--- db/migrate/20201016191026_create_projects.rb
+++ db/migrate/20201016191026_create_projects.rb
@@ -1,7 +1,10 @@
 class CreateProjects < ActiveRecord::Migration[6.0]
   def change
     create_table :projects do |t|
-
+      t.string :name
+      t.string :chain
+      t.string :category
+      t.float :held
       t.timestamps
     end
   end
